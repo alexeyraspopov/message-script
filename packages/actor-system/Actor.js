94a1897b7ca8b532c164ad516a20bc458515b3a8
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-12 17:17:28 -0500

    remove legacy implementation of Actor

--- packages/actor-system/Actor.js
+++ packages/actor-system/Actor.js
@@ -1,22 +1,6 @@
 export default class Actor {
   constructor(dispatcher) {
     this.dispatcher = dispatcher;
-    this.isDisabled = false;
-    this.enable();
-  }
-
-  async enable() {
-    this.isDisabled = false;
-
-    for await (const message of this.dispatcher) {
-      if (this.isDisabled) break;
-      this.receive(message);
-    }
-  }
-
-  disable() {
-    // should this mechanism be covered by different instance?
-    this.isDisabled = true;
   }
 
   receive() {
