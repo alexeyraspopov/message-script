e016df2c9ba2044d930cb1194fba5f6bf23db23b
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-12 17:15:09 -0500

    add export for DelayExecutor

--- packages/execution-context/index.js
+++ packages/execution-context/index.js
@@ -2,3 +2,4 @@ export ExecutionContext from './ExecutionContext';
 export AnimationFrameExecutor from './AnimationFrameExecutor';
 export IdleCallbackExecutor from './IdleCallbackExecutor';
 export ImmediateExecutor from './ImmediateExecutor';
+export DelayExecutor from './DelayExecutor';
