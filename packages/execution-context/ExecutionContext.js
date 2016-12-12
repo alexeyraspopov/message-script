5f499ac5cb14a407a2f1b345d14d5795727d85a3
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-12 10:18:32 +0100

    new implementation of ExecutionContext with concurrent and manual modes

--- packages/execution-context/ExecutionContext.js
+++ packages/execution-context/ExecutionContext.js
@@ -1,15 +1,39 @@
 export default class ExecutionContext {
-  constructor(executor) {
+  constructor(executor, concurrent = 1, manual = false) {
     this.executor = executor;
+    this.concurrent = concurrent;
+    this.manual = manual;
+    this.queue = [];
     this.current = Promise.resolve();
   }
 
-  async execute(routine) {
-    try {
-      await this.current;
-      this.current = this.executor.execute(routine);
-    } finally {
-      return this.current;
-    }
+  execute(routine) {
+    return new Promise((resolve, reject) => {
+      this.queue.push({ routine, resolve, reject });
+
+      if (!this.manual && this.queue.length === 1) {
+        this.enqueue();
+      }
+    });
   }
+
+  enqueue() {
+    return this.current.then(() => {
+      const batch = this.queue.splice(0, this.concurrent);
+      this.current = this.executor.execute(batchTasks(batch));
+      return this.queue.length === 0 || this.enqueue();
+    });
+  }
+}
+
+function batchTasks(tasks) {
+  return function batch() {
+    tasks.forEach(task => {
+      try {
+        task.resolve(task.routine());
+      } catch (error) {
+        task.reject(error);
+      }
+    });
+  };
 }
