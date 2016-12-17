8154d07f5ad07253409939e354815c0c0f3b5768
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-16 20:07:17 -0500

    make Message working cross-env

--- packages/message-dispatcher/Message.js
+++ packages/message-dispatcher/Message.js
@@ -1,16 +1,6 @@
 export default class Message {
   constructor(data) {
     this.data = data;
-  }
-
-  getSubject() {
-    return this.constructor;
-  }
-
-  toJSON() {
-    return {
-      type: this.constructor.name,
-      data: this.data
-    };
+    this.type = this.constructor.name;
   }
 }
