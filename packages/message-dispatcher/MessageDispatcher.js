72568aa1029f0695999835dedd9fa354cc1b9ccb
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-18 18:31:21 -0500

    broadcast message over mailboxes

--- packages/message-dispatcher/MessageDispatcher.js
+++ packages/message-dispatcher/MessageDispatcher.js
@@ -1,14 +1,19 @@
 import { ExecutionContext, ImmediateExecutor } from 'execution-context';
 
 export default class MessageDispatcher {
-  constructor(context) {
+  constructor(mailboxes, context) {
     if (!context) {
       const executor = new ImmediateExecutor();
       context = new ExecutionContext(executor);
     }
 
+    this.mailboxes = mailboxes;
     this.context = context;
   }
 
-  dispatch() { }
+  dispatch(message) {
+    for (const mailbox of this.mailboxes) {
+      mailbox.enqueue(message);
+    }
+  }
 }
