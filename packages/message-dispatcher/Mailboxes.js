b21e5ffd3e6902b37812abcd2a46e4431bd23d36
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-18 19:01:17 -0500

    add Mailboxes instance

---
+++ packages/message-dispatcher/Mailboxes.js
@@ -0,0 +1,5 @@
+export default class Mailboxes {
+  constructor() {
+    this.mailboxes = [];
+  }
+}
--- packages/message-dispatcher/index.js
+++ packages/message-dispatcher/index.js
@@ -2,3 +2,4 @@ export MessageDispatcher from './MessageDispatcher';
 export Message from './Message';
 export Mailbox from './Mailbox';
 export Envelope from './Envelope';
+export Mailboxes from './Mailboxes';
