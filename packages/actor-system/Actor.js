bb78c52a11a8368a7f35772167cba10c02c8d9a5
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-21 15:53:25 -0500

    implement tell method of ActorRef

--- packages/actor-system/Actor.js
+++ packages/actor-system/Actor.js
@@ -1,6 +1,7 @@
 export default class Actor {
   constructor(system, mailbox) {
     this.system = system;
+    this.mailbox = mailbox;
     this.subscription = mailbox.register(({ message, sender }) => {
       this.receive(message, sender);
     });
