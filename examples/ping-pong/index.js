14136d02712a9b7f31dff758c9058a1b284370a8
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-22 17:42:49 -0500

    simplify ping-pong example by removing unnecessary actor type

--- examples/ping-pong/index.js
+++ examples/ping-pong/index.js
@@ -3,23 +3,15 @@ import { Message } from 'message-dispatcher';
 
 class Ping extends Message { }
 
-class First extends Actor {
-  receive(message) {
-    const enemy = this.system.actorOf(Second, 'second');
-    console.log(`First player got ${message.data.count}`);
-    enemy.tell(new Ping({ count: message.data.count + 1 }), this);
-  }
-}
-
-class Second extends Actor {
-  receive(message) {
-    const enemy = this.system.actorOf(First, 'first');
-    console.log(`Second player got ${message.data.count}`);
-    enemy.tell(new Ping({ count: message.data.count + 1 }), this);
+class Player extends Actor {
+  receive(message, sender) {
+    console.log(`${this.name} got ${message.data.count}`);
+    sender.tell(new Ping({ count: message.data.count + 1 }), this);
   }
 }
 
 const system = new ActorSystem();
-const firstPlayer = system.actorOf(First, 'first');
+const firstPlayer = system.actorOf(Player, 'first');
+const secondPlayer = system.actorOf(Player, 'second');
 
-firstPlayer.tell(new Ping({ count: 0 }));
+firstPlayer.tell(new Ping({ count: 0 }), secondPlayer);
