27922f73e3bb0ae6cfd288362b5e1484922c7bb1
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-24 11:56:43 -0500

    freeze messages

--- packages/message-dispatcher/Message.js
+++ packages/message-dispatcher/Message.js
@@ -2,5 +2,7 @@ export default class Message {
   constructor(data) {
     this.subject = this.constructor.name;
     this.content = data;
+    Object.freeze(this);
+    Object.freeze(this.content);
   }
 }
