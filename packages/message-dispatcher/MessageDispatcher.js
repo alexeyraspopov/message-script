b88083b41742649a158f3191ed1b749af7152ee2
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-31 14:35:36 -0500

    move mailbox factory to message dispatcher

--- packages/message-dispatcher/MessageDispatcher.js
+++ packages/message-dispatcher/MessageDispatcher.js
@@ -1,10 +1,21 @@
 export default class MessageDispatcher {
-  constructor(mailboxes) {
-    this.mailboxes = mailboxes;
+  constructor() {
+    this.mailboxes = new Map();
+  }
+
+  mailboxOf(MailboxType) {
+    if (this.mailboxes.has(MailboxType)) {
+      return this.mailboxes.get(MailboxType);
+    }
+
+    const mailbox = new MailboxType();
+    this.mailboxes.set(MailboxType, mailbox);
+
+    return mailbox;
   }
 
   dispatch(envelope) {
-    for (const mailbox of this.mailboxes) {
+    for (const mailbox of this.mailboxes.values()) {
       mailbox.enqueue(envelope);
     }
   }
