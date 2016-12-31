eb47336de60471f8c1961885bb83ef067ade70ac
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-31 14:39:06 -0500

    update message-dispatcher entry point

--- packages/message-dispatcher/index.js
+++ packages/message-dispatcher/index.js
@@ -2,6 +2,5 @@ import MessageDispatcher from './MessageDispatcher';
 import Message from './Message';
 import Mailbox from './Mailbox';
 import Envelope from './Envelope';
-import Mailboxes from './Mailboxes';
 
-export { MessageDispatcher, Message, Mailbox, Envelope, Mailboxes };
+export { MessageDispatcher, Message, Mailbox, Envelope };
