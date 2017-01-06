0c587c77c27f231e33fb633486bd45ccc333e48c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-06 16:15:21 -0500

    bootstrap all necessary gateways

---
+++ packages/uniform-environment/ClusterGateway.js
@@ -0,0 +1,5 @@
+export default class ClusterGateway {
+  constructor(cluster) {
+    this.cluster = cluster;
+  }
+}
---
+++ packages/uniform-environment/SocketGateway.js
@@ -0,0 +1,5 @@
+export default class SocketGateway {
+  constructor(socket) {
+    this.socket = socket;
+  }
+}
