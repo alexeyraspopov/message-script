3d72ecf41e406af77fe5900efc07c4431cc98aec
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-05 22:58:34 -0500

    add Subscription to the module exports

--- packages/message-dispatcher/index.js
+++ packages/message-dispatcher/index.js
@@ -2,5 +2,6 @@ import MessageDispatcher from './MessageDispatcher';
 import Message from './Message';
 import Mailbox from './Mailbox';
 import Envelope from './Envelope';
+import Subscription from './Subscription';
 
-export { MessageDispatcher, Message, Mailbox, Envelope };
+export { MessageDispatcher, Message, Mailbox, Envelope, Subscription };
