30ce204eccd06216e641148197d6a30f5c5f2e97
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-05 23:01:05 -0500

    delete ActorSelection

--- packages/actor-system/index.js
+++ packages/actor-system/index.js
@@ -1,6 +1,5 @@
 import ActorSystem from './ActorSystem';
-import ActorSelection from './ActorSelection';
 import ActorRef from './ActorRef';
 import Actor from './Actor';
 
-export { ActorSystem, ActorSelection, ActorRef, Actor };
+export { ActorSystem, ActorRef, Actor };
