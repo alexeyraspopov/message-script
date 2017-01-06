d2b13a6d3339d846d1d563040e12296101d33c89
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-06 16:12:16 -0500

    bootstrap uniform-environment package

---
+++ packages/uniform-environment/README.md
@@ -0,0 +1,1 @@
+# Message Script / Uniform Environment
---
+++ packages/uniform-environment/index.js
@@ -0,0 +1,4 @@
+import UniformEnvironment from './UniformEnvironment';
+import WorkerGateway from './WorkerGateway';
+
+export { UniformEnvironment, WorkerGateway };
