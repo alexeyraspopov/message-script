0072b08e890b83faa491ea7ac3f224114814b493
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-06 16:10:51 -0500

    move WorkerGateway to the separate package

--- packages/actor-system/index.js
+++ packages/actor-system/index.js
@@ -1,6 +1,5 @@
 import ActorSystem from './ActorSystem';
 import ActorRef from './ActorRef';
 import Actor from './Actor';
-import WorkerGateway from './WorkerGateway';
 
-export { ActorSystem, ActorRef, Actor, WorkerGateway };
+export { ActorSystem, ActorRef, Actor };
