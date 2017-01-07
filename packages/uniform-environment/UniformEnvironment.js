ebff2aa4e42c183a801fdab4eb5c9a7cfa122f8c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-06 23:13:48 -0500

    define environment object's properties

--- packages/uniform-environment/UniformEnvironment.js
+++ packages/uniform-environment/UniformEnvironment.js
@@ -1,14 +1,19 @@
 export default class UniformEnvironment {
   constructor(children) {
-
+    this.children = children;
+    this.processes = new Map();
   }
 
   spawn(routine) {
+    const address = '';
 
+    this.processes.set(address, routine);
+
+    return address;
   }
 
   post(message) {
-    for (const child of children) {
+    for (const child of this.children) {
       child.send(message);
     }
   }
