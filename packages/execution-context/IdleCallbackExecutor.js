9a894a01d9728545b112baa07168d500a1e5e126
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-17 18:06:20 +0200

    make IdleCallbackExecutor working with queue

--- packages/execution-context/IdleCallbackExecutor.js
+++ packages/execution-context/IdleCallbackExecutor.js
@@ -3,9 +3,20 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/window/requestIdleCallback
  */
 export default class IdleCallbackExecutor {
-  execute(routine) {
-    return new Promise(resolve => {
-      requestIdleCallback(() => resolve(routine()));
+  execute(queue) {
+    return new Promise(resolveQueue => {
+      requestIdleCallback(deadline => {
+        while (deadline.timeRemaining() > 0 && queue.length > 0) {
+          const task = queue.shift();
+          task.run();
+        }
+
+        if (queue.length > 0) {
+          return resolveQueue(this.execute(queue));
+        }
+
+        resolveQueue();
+      });
     });
   }
 }
