f0c1c03dd91c0747ff9e21b38e5ef0417b0710da
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-17 18:05:49 +0200

    make ImmediateExecutor working with queue

--- packages/execution-context/ImmediateExecutor.js
+++ packages/execution-context/ImmediateExecutor.js
@@ -2,7 +2,16 @@
  * Schedules a routine execution on the next event loop
  */
 export default class ImmediateExecutor {
-  execute(routine) {
-    return new Promise(resolve => resolve(routine()));
+  execute(queue) {
+    return new Promise(resolveQueue => {
+      const task = queue.shift();
+      task.run();
+
+      if (queue.length > 0) {
+        return resolveQueue(Promise.resolve().then(() => this.execute(queue)));
+      }
+
+      resolveQueue();
+    });
   }
 }
