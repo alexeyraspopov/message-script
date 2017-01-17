4eaf64dd5b293efc71ad8b4b9e3e1c5b526fe265
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-17 17:59:13 +0200

    make NextTickExecutor working with queue

--- packages/execution-context/NextTickExecutor.js
+++ packages/execution-context/NextTickExecutor.js
@@ -3,9 +3,18 @@
  * https://nodejs.org/api/process.html#process_process_nexttick_callback_args
  */
 export default class NextTickExecutor {
-  execute(routine) {
-    return new Promise(resolve => {
-      process.nextTick(() => resolve(routine()));
+  execute(queue) {
+    return new Promise(resolveQueue => {
+      process.nextTick(() => {
+        const task = queue.shift();
+        task.run();
+
+        if (queue.length > 0) {
+          return resolveQueue(Promise.resolve().then(() => this.execute(queue)));
+        }
+
+        resolveQueue();
+      }, this.delay);
     });
   }
 }
