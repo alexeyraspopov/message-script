fc8fe49d237f0cbdaa96928042c60c0ffb28bec9
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-01-18 18:12:44 +0200

    allow ImmediateContext running more than one routine in the same loop

--- packages/execution-context/ImmediateExecutor.js
+++ packages/execution-context/ImmediateExecutor.js
@@ -2,10 +2,19 @@
  * Schedules a routine execution on the next event loop
  */
 export default class ImmediateExecutor {
+  /**
+   * @param {Number} concurrent — a number of routines that should be executed in the same loop
+   */
+  constructor(concurrent) {
+    this.concurrent = parseInt(concurrent) || 1;
+  }
+
   execute(queue) {
     return new Promise(resolveQueue => {
-      const task = queue.shift();
-      task.run();
+      for (let i = 0; i < this.concurrent; i++) {
+        const task = queue.shift();
+        task.run();
+      }
 
       if (queue.length > 0) {
         return resolveQueue(Promise.resolve().then(() => this.execute(queue)));
