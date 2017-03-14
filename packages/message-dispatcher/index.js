da4d4420b207a8c0aed22692245dfa15f34efe3a
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-14 09:40:24 +0200

    move message dispatcher modules to the separate folder

--- packages/message-dispatcher/index.js
+++ packages/message-dispatcher/index.js
@@ -1,7 +1,7 @@
-import MessageDispatcher from './MessageDispatcher';
-import Message from './Message';
-import Mailbox from './Mailbox';
-import Envelope from './Envelope';
-import Subscription from './Subscription';
+import MessageDispatcher from './modules/MessageDispatcher';
+import Message from './modules/Message';
+import Mailbox from './modules/Mailbox';
+import Envelope from './modules/Envelope';
+import Subscription from './modules/Subscription';
 
 export { MessageDispatcher, Message, Mailbox, Envelope, Subscription };
