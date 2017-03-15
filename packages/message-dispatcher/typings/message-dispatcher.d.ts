587ad65f6eedc2e2663ceb71832293b99968ad9a
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-15 23:53:15 +0200

    create typescript typing file for message-dispatcher

---
+++ packages/message-dispatcher/typings/message-dispatcher.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="execution-context" />
+
+declare module MessageDispatcher {
+  export class MessageDispatcher {
+    constructor();
+    mailboxOf(mailboxType: typeof Mailbox): Mailbox;
+    dispatch(envelope: Envelope): void;
+  }
+
+  export class Envelope {
+    message: Message<any>;
+    sender: any;
+    constructor(message: Message<any>, sender: any);
+  }
+
+  export class Message<T> {
+    subject: string;
+    content: T;
+    constructor(data: T);
+  }
+
+  export class Mailbox {
+    constructor(context: ExecutionContext.ExecutionContext);
+    enqueue(envelope: Envelope): void;
+    register(receiver: Function): Subscription;
+  }
+
+  export class Subscription {
+    dispose(): void;
+  }
+}
