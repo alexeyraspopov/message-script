c081e3444d9ec854dae746e62547d2cccbb3964d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-15 22:57:24 +0200

    remove remaining jsdoc comments

--- packages/message-dispatcher/modules/MessageDispatcher.js
+++ packages/message-dispatcher/modules/MessageDispatcher.js
@@ -3,11 +3,6 @@ export default class MessageDispatcher {
     this.mailboxes = new Map();
   }
 
-  /**
-   * Creates a singleton mailbox based on its constructor.
-   * @param {Class} Mailbox — a particular mailbox class
-   * @return {Mailbox} — an instance of provided mailbox class
-   */
   mailboxOf(MailboxType) {
     if (this.mailboxes.has(MailboxType)) {
       return this.mailboxes.get(MailboxType);
@@ -19,10 +14,6 @@ export default class MessageDispatcher {
     return mailbox;
   }
 
-  /**
-   * Broadcasts an envelope across registered mailboxes
-   * @param {Envelope} envelope — pair of message and sender
-   */
   dispatch(envelope) {
     for (const mailbox of this.mailboxes.values()) {
       mailbox.enqueue(envelope);
