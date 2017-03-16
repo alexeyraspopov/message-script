5e21024e33155c5dfc67c3adfbd88c51b099aeb6
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-16 08:49:52 +0200

    integrate message dispatcher to actor system

--- packages/actor-system/ActorSystem.js
+++ packages/actor-system/ActorSystem.js
@@ -1,33 +1,33 @@
-import { MessageDispatcher } from 'message-dispatcher';
+import { MessageDispatcher, Envelope } from 'message-dispatcher';
 import ActorRef from './ActorRef';
 
 export default class ActorSystem {
-  constructor() {
+  constructor(dispatcher) {
     this.actors = new Map();
-    this.dispatcher = new MessageDispatcher();
+    this.dispatcher = dispatcher;
   }
 
-  getStats() {
-    const entries = this.actors.entries();
-    const serialize = ([name, actor]) => ({
-      name,
-      type: actor.constructor.name,
-      actor
-    });
-
-    return Array.from(entries, serialize);
-  }
-
-  actorOf(ActorConstructor, name) {
+  actorOf(ActorType, name = ActorType.name.toLowerCase()) {
     if (this.actors.has(name)) {
       return this.actors.get(name);
     }
 
-    const actor = new ActorConstructor(this);
+    const actor = new ActorType(this);
     const ref = new ActorRef(actor);
+
+    const mailbox = this.dispatcher.mailboxOf(ActorType.getMailboxType());
+
+    mailbox.register(({ message, sender }) => {
+      actor.receive(message, sender);
+    });
 
     this.actors.set(name, ref);
 
     return actor;
+  }
+
+  dispatch(message, sender) {
+    const envelope = new Envelope(message, sender);
+    this.dispatcher.dispatch(envelope);
   }
 }
