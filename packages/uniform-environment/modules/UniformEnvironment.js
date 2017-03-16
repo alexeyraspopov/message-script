009c7e4c987691031a505665f3de286a885c2a74
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-16 19:19:43 +0200

    remove unused things

--- packages/uniform-environment/modules/UniformEnvironment.js
+++ packages/uniform-environment/modules/UniformEnvironment.js
@@ -14,11 +14,10 @@ export default class UniformEnvironment {
   constructor(children, routine) {
     this.children = children;
     this.routine = routine;
-    this.processes = new Map();
   }
 
   post(message) {
-    for (const child of this.children.values()) {
+    for (const child of this.children) {
       child.send(message);
     }
   }
