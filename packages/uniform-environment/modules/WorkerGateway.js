7e8faa9cf2320094dfae3d075289bd4cceb03b19
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-16 09:07:36 +0200

    create descriptive typings for uniform-enviroment

--- packages/uniform-environment/modules/WorkerGateway.js
+++ packages/uniform-environment/modules/WorkerGateway.js
@@ -1,10 +1,4 @@
-/**
- * Universal interface for communicaton between master and workers
- */
 export default class WorkerGateway {
-  /**
-   * @param {Worker} worker — worker instance or `self` to run inside worker
-   */
   constructor(worker) {
     this.worker = worker;
   }
--- packages/uniform-environment/typings/uniform-environment.d.ts
+++ packages/uniform-environment/typings/uniform-environment.d.ts
@@ -1,6 +1,32 @@
 declare module UniformEnvironment {
-  export class UniformEnvironment {}
-  export class ClusterGateway {}
-  export class SocketGateway {}
-  export class WorkerGateway {}
+  interface Gateway {
+    send(message: any): void;
+    listen(handler: Function): void;
+  }
+
+  export class UniformEnvironment {
+    constructor(children: Array<Gateway>, routine: Function);
+    post(message: any): void;
+  }
+
+  export class ClusterGateway implements Gateway {
+    constructor();
+    send(message: any): void;
+    listen(handler: Function): void;
+  }
+
+  export class SocketGateway implements Gateway {
+    constructor(socket: WebSocket);
+    send(message: any): void;
+    listen(handler: Function): void;
+  }
+
+  /**
+   * Universal interface for communicaton between master and workers
+   */
+  export class WorkerGateway implements Gateway {
+    constructor(worker: Worker);
+    send(message: any): void;
+    listen(handler: Function): void;
+  }
 }
