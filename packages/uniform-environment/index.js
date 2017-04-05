440f0be8e2a91936f9c59a750aba1dd859fd3658
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-05 23:41:10 +0300

    update entry point

--- packages/uniform-environment/index.js
+++ packages/uniform-environment/index.js
@@ -2,3 +2,4 @@ export { default as UniformEnvironment } from './modules/UniformEnvironment';
 export { default as WorkerGateway } from './modules/WorkerGateway';
 export { default as ClusterGateway } from './modules/ClusterGateway';
 export { default as SocketGateway } from './modules/SocketGateway';
+export { default as TabGateway } from './modules/TabGateway';
