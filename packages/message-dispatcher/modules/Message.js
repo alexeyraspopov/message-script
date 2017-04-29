7c0a14b51496ef8f7f22706015945a7ff0182a0f
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-29 18:48:06 +0300

    don't freeze messages for the sake of performance

--- packages/message-dispatcher/modules/Message.js
+++ packages/message-dispatcher/modules/Message.js
@@ -2,7 +2,5 @@ export default class Message {
   constructor(data) {
     this.subject = this.constructor.name;
     this.content = data;
-    Object.freeze(this);
-    Object.freeze(this.content);
   }
 }
